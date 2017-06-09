44a8620765d6718b8153d8c138678bdd593a0aa1
Revo <belbeche.w@gmail.com>
2017-06-09 09:24:59 +0200

    Create config.php

--- config.php
+++ config.php
@@ -15,10 +15,4 @@ error_reporting(0);
 
 	// Parametre Connexion
 
-	define('DB_SERVER', 'db683412430.db.1and1.com');
-	define('DB_USER' , 'dbo683412430');
-	define('DB_PASSWORD', 'dubosson123');
-	define('DB_NAME', 'db683412430');
 
-	define('FROM_EMAIL', 'belbeche.W@gmail.com');
-	define('FROM_NAME', 'Votre email !');
